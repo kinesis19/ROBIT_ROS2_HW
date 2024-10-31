19c431b818b504baf807c3407047b29ecb42bdd4
kinesis19 <lordkinesis@naver.com>
2024-10-31 10:51:25 +0900

    :hammer: Create: Day_2 HW_001 package

---
+++ DAY_2/HW_001/qt_turtlesim_controller/include/qt_turtlesim_controller/qnode.hpp
@@ -0,0 +1,43 @@
+/**
+ * @file /include/qt_turtlesim_controller/qnode.hpp
+ *
+ * @brief Communications central!
+ *
+ * @date February 2011
+ **/
+/*****************************************************************************
+** Ifdefs
+*****************************************************************************/
+
+#ifndef qt_turtlesim_controller_QNODE_HPP_
+#define qt_turtlesim_controller_QNODE_HPP_
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+#ifndef Q_MOC_RUN
+#include <rclcpp/rclcpp.hpp>
+#endif
+#include <QThread>
+
+/*****************************************************************************
+** Class
+*****************************************************************************/
+class QNode : public QThread
+{
+  Q_OBJECT
+public:
+  QNode();
+  ~QNode();
+
+protected:
+  void run();
+
+private:
+  std::shared_ptr<rclcpp::Node> node;
+
+Q_SIGNALS:
+  void rosShutDown();
+};
+
+#endif /* qt_turtlesim_controller_QNODE_HPP_ */
