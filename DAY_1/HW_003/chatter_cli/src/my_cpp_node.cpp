73f8754105243f861d7a6b97b204467d24c930da
kinesis19 <lordkinesis@naver.com>
2024-10-31 16:12:22 +0900

    :hammer: Create: chatter_cli

---
+++ DAY_1/HW_003/chatter_cli/src/my_cpp_node.cpp
@@ -0,0 +1,19 @@
+#include "rclcpp/rclcpp.hpp"
+
+class MyCppNode : public rclcpp::Node
+{
+public:
+    MyCppNode() : Node("my_cpp_node")
+    {
+        RCLCPP_INFO(this->get_logger(), "Hello, ROS 2 C++ Node!");
+    }
+};
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<MyCppNode>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
